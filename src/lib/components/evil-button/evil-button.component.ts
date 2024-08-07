4d616a1383ed5ea206b3d6af09678eca4036f9c7
Evilbas <evilbas@evilbas.com>
2024-08-07 13:51:48 +0800

    metal buttons

--- src/lib/components/evil-button/evil-button.component.ts
+++ src/lib/components/evil-button/evil-button.component.ts
@@ -22,6 +22,7 @@ export class EvilButtonComponent {
     | 'white' = 'red';
   @Input('w-full') wFull: boolean = false;
   @Input('link') link: string | undefined = undefined;
+  @Input('type') type: 'neon' | 'metal' = 'neon';
   @Output('click') clickHandler: EventEmitter<void> = new EventEmitter<void>();
   handleClick() {
     if (this.link) this.router.navigate([this.link]);
@@ -30,8 +31,10 @@ export class EvilButtonComponent {
 
   protected getClass(): string[] {
     const classes = [];
-    classes.push(`${this.color}`);
+
     classes.push(this.wFull ? 'w-full' : 'w-fit');
+    if (this.type === 'neon') classes.push(`${this.color}`, 'from-neutral-800');
+    if (this.type === 'metal') classes.push(`${this.color}-metal`, 'to-75%');
     return classes;
   }
 }
